a5eaa85cd20408a61c9f2113e9cf409125029ba8
Amber Bodnar <amber.bodnar@gmail.com>
2022-09-19 21:39:43 -0400

    Add ArticleDetails component

---
+++ src/Components/ArticleDetails/ArticleDetails.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ArticleDetails.css";
+
+const ArticleDetails = () => {
+  return (
+    <div className="article-details-container"></div>
+  );
+};
+
+export default ArticleDetails;

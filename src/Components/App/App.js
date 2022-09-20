00f22cd2a8faf8927c51b1f5781171cd00d4878b
Amber Bodnar <amber.bodnar@gmail.com>
2022-09-20 08:48:53 -0400

    Add useEffect for Top Stories

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "../Header/Header";
 import SearchForm from "../SearchForm/SearchForm";
@@ -6,6 +6,18 @@ import Articles from "../Articles/Articles";
 import Footer from "../Footer/Footer";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  // On page load, see Top News Stories
+  useEffect(() => {
+    fetch("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=GzKTz5zfmdAAPS5iUHTtNA4Vr9FGl8bE")
+     .then(res => res.json())
+     .then(data => {
+      setData(data.results)
+      console.log(data.results)
+     })
+  }, []);
+
   return (
     <div className="App">
       <Header />

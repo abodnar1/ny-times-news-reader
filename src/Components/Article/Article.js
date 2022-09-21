ed2e2aa1d60c3b3ef2a09986ceed0689d7a91a19
Amber Bodnar <amber.bodnar@gmail.com>
2022-09-21 08:25:17 -0400

    Add label to Form for accessibility

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -6,7 +6,7 @@ const Article = ({ id, section, title, published, image }) => {
   return (
     <Link to={`/article/${id}`} className="link-to-details">
       <article className="article-wrapper">
-        <img className="article-image" src={image} alt={section}/>
+        <img className="article-image" src={image} alt={title}/>
         <div className="info-wrapper">
           <h2 className="article-title">{title}</h2>
           <h3 className="article-section">Section: {section}</h3>

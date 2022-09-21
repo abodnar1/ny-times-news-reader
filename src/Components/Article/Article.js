32fb35c5b91b84c0c179c3c425d4af30ae52e3cb
Amber Bodnar <amber.bodnar@gmail.com>
2022-09-20 20:29:21 -0400

    Add box shadow/transform to Article component

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -4,7 +4,7 @@ import "./Article.css";
 const Article = ({ id, section, title, published, image }) => {
   return (
     <article className="article-wrapper">
-      <img className="article-image" src={image} alt="top story"/>
+      <img className="article-image" src={image} alt={section}/>
       <div className="info-wrapper">
         <h2 className="article-title">{title}</h2>
         <h3 className="article-section">Section: {section}</h3>

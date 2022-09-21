7f46b4a00b007437662e903eb26e4d1d0029076e
Amber Bodnar <amber.bodnar@gmail.com>
2022-09-20 20:17:51 -0400

    Add error message and loading to render

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,30 +8,39 @@ import ArticleDetails from "../ArticleDetails/ArticleDetails";
 
 const App = () => {
   const [data, setData] = useState([]);
-  const [category, setCategory] = useState("home")
+  const [category, setCategory] = useState("home");
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
   useEffect(() => {
+    setLoading(true);
+    
     fetch(`https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=GzKTz5zfmdAAPS5iUHTtNA4Vr9FGl8bE`)
      .then(res => {
-      if (res.status === 200) {
-        return res.json();
-      } else {
-        setError("Oops, something went wrong. Please try again.");
-      }
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          setError("Oops, something went wrong. Please try again.");
+        }
      })
      .then(data => {
       setData(data.results)
-      console.log(data.results)
+      setLoading(false)
+      // console.log(data.results)
      })
+     .catch(error => {
+      setError(error.message)
+      setLoading(false)
+    })
   }, [category]);
 
   return (
     <div className="App">
       <Header />
       <SearchForm setCategory={setCategory} />
-      <ArticleDetails />
-      <Articles data={data}/>
+      {error && <p className="error-message">{error}</p>}
+      {!loading ? <Articles data={data}/> : <p className="loading">Loading...</p>}
+      {/* <ArticleDetails /> */}
       <Footer />
     </div>
   );

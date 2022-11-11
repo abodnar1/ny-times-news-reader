6f273978b93ae2f9a4d890f02214adabf220b19a
Amber Bodnar <amber.bodnar@gmail.com>
2022-11-11 13:43:40 -0500

    Add unavailable image to ternary

--- src/Components/Articles/Articles.js
+++ src/Components/Articles/Articles.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Articles.css";
 import Article from "../Article/Article";
+import unavailableImage from "../../assets/unavilable.png";
 
 const Articles = ({ articles }) => {
   
   const mappedArticles = articles.map(article => {
-    let thumbnailImage = article.multimedia ? article.multimedia.find(image => image.format === "Large Thumbnail").url : null;
+    let thumbnailImage = article.multimedia ? article.multimedia.find(image => image.format === "Large Thumbnail").url : unavailableImage;
     return (
       <Article 
         id={article.created_date}

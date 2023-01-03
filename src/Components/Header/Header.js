9ebbeb97861dfef809c73723cc5689158678dad9
Amber Bodnar <amber.bodnar@gmail.com>
2023-01-03 15:31:19 -0500

    Implement new Intl.DateTimeFormat for Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,6 +5,11 @@ import "./Header.css";
 const Header = () => {
 
   const today = new Date();
+  const intlDate = new Intl.DateTimeFormat('en-us', {
+    dateStyle: "full",
+  });
+ const formattedDate = intlDate.format(today);
+
   const hours = today.getHours();
   let greeting;
   
@@ -21,7 +26,7 @@ const Header = () => {
       <Link to="/" className="home-link">
         <h1 className="site-title">NY Times News Reader</h1>
       </Link>
-      <p className="greeting">{`${greeting}`} Today is {`${today.toString().slice(0, 16)}`}</p>
+      <p className="greeting">{`${greeting}`} Today is {formattedDate}</p>
     </header>
   );
 };
